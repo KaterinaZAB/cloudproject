bcd13a48508139db6fd0fa61af87d0ed1b31006b
KaterinaZAB <59415539+KaterinaZAB@users.noreply.github.com>
2024-12-02 22:09:12 +0300

    commit

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from flask_caching.backends import SimpleCache
 from flask_caching import Cache  # Импортируем модуль для кэширования
 import os
 
-
-
 # Инициализация Flask приложения
 app = Flask(__name__)
 
